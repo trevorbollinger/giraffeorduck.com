10fd94d955a8423eaefdf7ff37e7c7b612ae0bc3
Trevor Bollinger <tbollinger@unomaha.edu>
2024-11-07 19:20:42 -0600

    styling

--- backend/giraffeorduck/settings.py
+++ backend/giraffeorduck/settings.py
@@ -43,8 +43,8 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "ACCESS_TOKEN_LIFETIME": timedelta(hours=3),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=3),
 }
 
 # Application definition
